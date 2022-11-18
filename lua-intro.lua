1e32bf35e5573cbe16e2fe1e8aaeed2d3e238132
doczi-dominik <doczi.dominik0@gmail.com>
2022-11-18 03:19:26 +0100

    📝 Updated lua-intro with types and better OOP

--- lua-intro.lua
+++ lua-intro.lua
@@ -88,12 +88,17 @@ end
 
 -- [[ FÜGGVÉNYEK  ]]
 
+---Köszön
+---@param nev string
 local function koszones(nev)
     print("Hello", nev)
 end
 
 koszones("Peti")
 
+---@param x number
+---@return number
+---@return number
 local function trig(x)
     return math.sin(x), math.cos(x)
 end
@@ -103,6 +108,9 @@ local sin, cos = trig(90)
 print(sin)  -- 1
 print(cos)  -- 0
 
+---@param x number
+---@return number
+---@return number
 local trig2 = function(x)
     return math.tan(x), math.tanh(x)
 end
@@ -110,7 +118,7 @@ end
 
 -- [[ TÁBLÁK ]]
 
-local lista_forma = {"egy, ketto", "harom"}
+local lista_forma = {"egy", "ketto", "harom"}
 
 -- PRIME BULLSHIT #6
 -- Indexelés 1-től kezdődik
@@ -151,26 +159,65 @@ print(elbaszott[false])  -- NE
 
 --[[ """" OOP """" ]]
 
-local kisbetus_koszono = {
-    nev = "Peti",
-    koszon = function(k)
-        print("cs", k.nev)
-    end
-}
+---@class IRajzolhato
+---@field rajzol fun(this: IRajzolhato)
 
-local nagybetus_koszono = {
-    nev = "Pisti",
-    koszon = function(k)
-        print("SZIAAAA", k.nev)
-    end
-}
+---@class Kor : IRajzolhato
+---@field x number
+---@field y number
+---@field r number
 
-local koszono
+---@param x number
+---@param y number
+---@param r number
+---@return Kor
+local function UjKor(x, y, r)
+    return {
+        x = x,
+        y = y,
+        r = r,
+        ---@param this Kor
+        rajzol = function(this)
+        end
+    }
+end
 
-koszono = kisbetus_koszono
-koszono.koszon(koszono) -- cs Peti
+---@class Haromszog : IRajzolhato
+---@field x1 number
+---@field y1 number
+---@field x2 number
+---@field y2 number
+---@field x3 number
+---@field y3 number
 
-koszono = nagybetus_koszono
-koszono.koszon(koszono) -- SZIAAAA Pisti
+---@param x1 number
+---@param y1 number
+---@param x2 number
+---@param y2 number
+---@param x3 number
+---@param y3 number
+---@return Haromszog
+local function UjHaromSzog(x1, y1, x2, y2, x3, y3)
+    return {
+        x1 = x1,
+        y1 = y1,
+        x2 = x2,
+        y2 = y2,
+        x3 = x3,
+        y3 = y3,
+        ---@param this Haromszog
+        rajzol = function(this)
+        end
+    }
+end
 
-koszono:koszon()
+---@type IRajzolhato
+local rajzolo
+
+rajzolo = UjKor(64, 64, 128)
+rajzolo.rajzol(rajzolo)
+
+rajzolo = UjHaromSzog(16, 16, 64, 16, 32, 32)
+rajzolo.rajzol(rajzolo)
+
+rajzolo:rajzol()
